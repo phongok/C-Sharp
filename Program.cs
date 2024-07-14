b7d483afa5eae247ff2121ccf6bffccd87f8531b
phongok <phongdevgithub666666@gmail.com>
2024-07-14 20:40:01 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Variables = Basic.Variables;
+
+namespace Basic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Variable
+            Console.WriteLine("Hello World");
+            Variables.Variable variable = new Variables.Variable();
+            variable.ConvertToInt_Option1();
+            variable.ConvertToInt_Option2();
+            variable.ConvertToInt_Option3();
+
+
+
+            
+          }
+    }
+}
---
+++ Variables/Variable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.Variables
+{
+    public class Variable
+    {
+
+      public void ConvertToInt_Option1()
+        {
+            string a = "123";
+            int outphut_a = Convert.ToInt32(a);
+            Console.WriteLine(outphut_a);
+        }
+
+        public void ConvertToInt_Option2()
+        {
+            string a = "1234";
+            int outphut_a = int.Parse(a);
+            Console.WriteLine(outphut_a);
+        }
+
+        public void ConvertToInt_Option3()
+        {
+            string a = "1234a";
+            bool success = int.TryParse(a, out int outphut_a);
+            if (success)
+            {
+                Console.WriteLine(outphut_a);
+            }
+            else
+            {
+                Console.WriteLine("Convert failed");
+            }
+        }
+    }
+}
